3a09d1f9dc28b859e714adee79587c39c03f6a90
Chris Impicciche <peechiz@bakudan.local>
2016-08-10 13:32:54 -0500

    trying to figure out heroku madness with fun comments

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,10 @@ var express = require('express'),
 router.route('/')
 //get all users
   .get((req,res)=>{
+    console.log('GOT TO THE ROUTE, GOING TO DB!!!');
     db.get('users', function(err, users){
+      console.log('OK ABOUT TO SEND SOME JSON');
+      console.log('IT SHOULD LOOK LIKE', users);
       res.json(users);
     })
   })
