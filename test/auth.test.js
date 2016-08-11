fb151713876fd64306370f2a7c3e68bb2cef4044
Chris Impicciche <peechiz@bakudan.local>
2016-08-11 11:37:15 -0500

    adds empty auth route files

---
+++ test/auth.test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let app = require('../server.js');
+const expect = require('chai').expect;
+const request = require('supertest')(app);
+let db = require('../level/level.js').db;
+
+describe('AUTH TESTS', function(){
+
+  
+
+})
